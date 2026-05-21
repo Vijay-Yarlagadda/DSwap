e68cc7e0758f559e49321fec53fec3a9ffc1499a
Vijay-Yarlagadda <yarlagaddavijay859@gmail.com>
2026-05-21 10:04:07 +0530

    commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,15 +3,17 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'AIzaSyCvmBIiaQMdmVzZqLR7VP06hONrrhFloIo',
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'dswap-6d834.firebaseapp.com',
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || 'dswap-6d834',
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || '1:548215971868:web:160fa936d6e05a0eca3fd9',
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '548215971868',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
 export default app;
